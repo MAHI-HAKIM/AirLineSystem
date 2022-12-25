80ded7f76809f462ae7fc4b8815e2295769fc619
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2022-12-25 18:31:01 +0300

    Guna package Installed

--- AirLineSystem/Login.Designer.cs
+++ AirLineSystem/Login.Designer.cs
@@ -29,10 +29,18 @@ namespace AirLineSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(764, 540);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "LogIn";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
