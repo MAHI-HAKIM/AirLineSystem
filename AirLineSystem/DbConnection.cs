faf5760ce0ddfb7580a805e7a24b993b81f60e30
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2022-12-25 03:50:46 +0300

    Added DbConnection Class

--- AirLineSystem/DbConnection.cs
+++ AirLineSystem/DbConnection.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace AirLineSystem
 {
     class DbConnection
     {
+        private NpgsqlConnection connection = new NpgsqlConnection("server=localhost; port = 5432; DataBase = AirLineDBSystem; user Id = postgres ; password = Mahi.2003");
+        public NpgsqlConnection GetCon()
+        {
+            return connection;
+        }
+        public void OpenCon()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        public void CloseCon()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
     }
 }
